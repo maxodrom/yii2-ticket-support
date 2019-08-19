6c90657b8d972852a465935ac43e67cf58615c10
Max Alexandrov <max@7u3.ru>
2019-08-19 16:57:16 +0300

    Update code style. Use 'maxodrom' as vendor.
    Signed-off-by: Max Alexandrov <max@7u3.ru>

--- assets/TicketAsset.php
+++ assets/TicketAsset.php
@@ -4,8 +4,13 @@ namespace akiraz2\support\assets;
 
 use yii\web\AssetBundle;
 
-class TicketAsset extends AssetBundle {
-    public $sourcePath = '@vendor/akiraz2/yii2-ticket-support/assets/default';
+/**
+ * Class TicketAsset
+ * @package akiraz2\support\assets
+ */
+class TicketAsset extends AssetBundle
+{
+    public $sourcePath = '@vendor/maxodrom/yii2-ticket-support/assets/default';
     public $baseUrl = '@web';
     public $css = [
         'css/ticket-style.css',
@@ -14,6 +19,6 @@ class TicketAsset extends AssetBundle {
     ];
     public $depends = [
         'yii\bootstrap\BootstrapAsset',
-        '\rmrevin\yii\fontawesome\AssetBundle'
+        '\rmrevin\yii\fontawesome\AssetBundle',
     ];
 }
