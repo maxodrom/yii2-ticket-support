0628e1ad6bc99ecb48d3fd82857b475d7262f6d3
Max Alexandrov <max@7u3.ru>
2019-08-19 16:22:25 +0300

    Some refactoring on migration. Fix code style.
    Signed-off-by: Max Alexandrov <max@7u3.ru>

--- migrations/m180925_080718_init.php
+++ migrations/m180925_080718_init.php
@@ -26,48 +26,77 @@ class m180925_080718_init extends Migration
         }
 
         $this->createTable('{{%support_category}}', [
-            'id' => $this->primaryKey(),
+            'id' => $this->primaryKey(11)->unsigned(),
             'title' => $this->string()->notNull(),
             'status' => $this->tinyInteger()->notNull(),
-            'created_at' => $this->integer()->notNull(),
-            'updated_at' => $this->integer()->notNull(),
+            'created_at' => $this->integer()->notNull()->unsigned(),
+            'updated_at' => $this->integer()->notNull()->unsigned(),
         ], $tableOptions);
 
         $this->createTable('{{%support_ticket_head}}', [
-            'id' => $this->primaryKey(),
+            'id' => $this->primaryKey(11)->unsigned(),
             'hash_id' => $this->string()->notNull()->unique(),
-            'category_id' => $this->integer()->null()->defaultValue(null),
+            'category_id' => $this->integer()->null()->defaultValue(null)->unsigned(),
             'title' => $this->string()->notNull(),
             'type_id' => $this->tinyInteger()->notNull(),
             'user_contact' => $this->string()->notNull(),
             'user_name' => $this->string()->null()->defaultValue(null),
             'status' => $this->tinyInteger()->notNull(),
             'priority' => $this->tinyInteger()->null()->defaultValue(null),
-            'user_id' => $this->integer()->null()->defaultValue(null),
-            'created_at' => $this->integer()->notNull(),
-            'updated_at' => $this->integer()->notNull(),
+            'user_id' => $this->integer()->null()->defaultValue(null)->unsigned(),
+            'created_at' => $this->integer()->notNull()->unsigned(),
+            'updated_at' => $this->integer()->notNull()->unsigned(),
         ], $tableOptions);
-        $this->addForeignKey('{{%fk_support_ticket_head_user_id-user_id}}', '{{%support_ticket_head}}', 'user_id',
-            $this->getModule()->userModel::tableName(), $this->getModule()->userPK, 'CASCADE', 'CASCADE');
-        $this->addForeignKey('{{%fk_support_ticket_head_category-support_category_id}}', '{{%support_ticket_head}}',
+
+        $this->addForeignKey(
+            '{{%fk_support_ticket_head_user_id-user_id}}',
+            '{{%support_ticket_head}}',
+            'user_id',
+            $this->getModule()->userModel::tableName(),
+            $this->getModule()->userPK,
+            'CASCADE',
+            'CASCADE'
+        );
+
+        $this->addForeignKey(
+            '{{%fk_support_ticket_head_category-support_category_id}}',
+            '{{%support_ticket_head}}',
             'category_id',
-            '{{%support_category}}', 'id');
+            '{{%support_category}}',
+            'id'
+        );
 
         $this->createTable('{{%support_ticket_content}}', [
-            'id' => $this->primaryKey(),
-            'id_ticket' => $this->integer()->notNull(),
+            'id' => $this->primaryKey(11)->unsigned(),
+            'id_ticket' => $this->integer()->notNull()->unsigned(),
             'content' => $this->text()->notNull(),
             'info' => $this->text()->null()->defaultValue(null),
             'mail_id' => $this->text()->null()->defaultValue(null),
             'fetch_date' => $this->text()->null()->defaultValue(null),
-            'user_id' => $this->integer()->null()->defaultValue(null),
-            'created_at' => $this->integer()->notNull(),
-            'updated_at' => $this->integer()->notNull(),
+            'user_id' => $this->integer()->null()->defaultValue(null)->unsigned(),
+            'created_at' => $this->integer()->notNull()->unsigned(),
+            'updated_at' => $this->integer()->notNull()->unsigned(),
         ], $tableOptions);
-        $this->addForeignKey('{{%fk_support_ticket_content_id-support_ticket_head_id}}', '{{%support_ticket_content}}',
-            'id_ticket', '{{%support_ticket_head}}', 'id', 'CASCADE', 'CASCADE');
-        $this->addForeignKey('{{%fk_support_ticket_content_user_id-user_id}}', '{{%support_ticket_content}}',
-            'user_id', $this->getModule()->userModel::tableName(), $this->getModule()->userPK, 'CASCADE', 'CASCADE');
+
+        $this->addForeignKey(
+            '{{%fk_support_ticket_content_id-support_ticket_head_id}}',
+            '{{%support_ticket_content}}',
+            'id_ticket',
+            '{{%support_ticket_head}}',
+            'id',
+            'CASCADE',
+            'CASCADE'
+        );
+
+        $this->addForeignKey(
+            '{{%fk_support_ticket_content_user_id-user_id}}',
+            '{{%support_ticket_content}}',
+            'user_id',
+            $this->getModule()->userModel::tableName(),
+            $this->getModule()->userPK,
+            'CASCADE',
+            'CASCADE'
+        );
     }
 
     /**
@@ -75,17 +104,24 @@ class m180925_080718_init extends Migration
      */
     public function down()
     {
-        $this->dropForeignKey('{{%fk_support_ticket_content_user_id-core_account_id}}',
-            '{{%support_ticket_content}}');
-        $this->dropForeignKey('{{%fk_support_ticket_content_id-support_ticket_head_id}}',
-            '{{%support_ticket_content}}');
+        $this->dropForeignKey(
+            '{{%fk_support_ticket_content_user_id-core_account_id}}',
+            '{{%support_ticket_content}}'
+        );
+        $this->dropForeignKey(
+            '{{%fk_support_ticket_content_id-support_ticket_head_id}}',
+            '{{%support_ticket_content}}'
+        );
         $this->dropTable('{{%support_ticket_content}}');
-
-        $this->dropForeignKey('{{%fk_support_ticket_head_category-support_category_id}}', '{{%support_ticket_head}}');
-        $this->dropForeignKey('{{%fk_support_ticket_head_user_id-core_account_id}}', '{{%support_ticket_head}}');
+        $this->dropForeignKey(
+            '{{%fk_support_ticket_head_category-support_category_id}}',
+            '{{%support_ticket_head}}'
+        );
+        $this->dropForeignKey(
+            '{{%fk_support_ticket_head_user_id-core_account_id}}',
+            '{{%support_ticket_head}}'
+        );
         $this->dropTable('{{%support_ticket_head}}');
-
         $this->dropTable('{{%support_category}}');
     }
-
 }
